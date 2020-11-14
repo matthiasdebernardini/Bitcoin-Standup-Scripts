26a20cc5863bcc2ce78b9e57712c2467e5096087
keblek <68111648+keblek@users.noreply.github.com>
2020-11-14 01:23:29 +0100

    Updates Bitoin Core on ubuntu

---
+++ Scripts/update_bitcoin.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+# DISCLAIMER: It is not a good idea to store large amounts of Bitcoin on a VPS,
+# ideally you should use this as a watch-only wallet. This script is expiramental
+# and has not been widely tested. The creators are not responsible for loss of
+# funds. If you are not familiar with running a node or how Bitcoin works then we
+# urge you to use this in testnet so that you can use it as a learning tool.
+
+# This script installs the latest stable version of Bitcoin Core and ubuntu updates
+
+# StandUp.sh will create two logs in your root directory, to read them run:
+# $ cat standup.err
+# $ cat standup.log
+
+####
+#0. Prerequisites
+####
+
+# In order to run this script you need to be logged in as root, and enter in the commands
+# listed below:
+
+# (the $ represents a terminal commmand prompt, do not actually type in a $)
+
+# First you need to give the root user a password:
+# $ sudo passwd
+
+# Then you need to switch to the root user:
+# $ su - root
+
+# Then create the file for the script:
+# $ nano standup.sh
+
+# Nano is a text editor that works in a terminal
+# Replace the version number for Bitcoin Core 
+# then you can type:
+# control x (this starts to exit nano)
+# y (this confirms you want to save the file)
+# return (just press enter to confirm you want to save and exit)
+
+# Then we need to make sure the script can be executable with:
+# $ chmod +x standup.sh
+
+# After that you can run the script with the optional arguments like so:
+# $ ./standup.sh "insert pubkey" "insert node type (see options below)" "insert ssh key" "insert ssh allowed IP's" "insert password for standup user"
+
+
+# Force check for root, if you are not logged in as root then the script will not execute
+if ! [ "$(id -u)" = 0 ]
+then
+
+  echo "$0 - You need to be logged in as root!"
+  exit 1
+
+fi
+
+# Output stdout and stderr to ~root files
+exec > >(tee -a /root/standup-bitcoin-update.log) 2> >(tee -a /root/standup-bitcoin-update.log /root/standup-bitcoin-update.err >&2)
+
+####
+# 2. Bring Debian Up To Date
+####
+
+echo "$0 - Starting Debian updates; this will take a while!"
+
+# Make sure all packages are up-to-date
+apt update
+apt upgrade -y
+
+echo "$0 - Updated APT Packages"
+
+####
+# 5. Install Bitcoin
+####
+
+# Download Bitcoin
+echo "$0 - Downloading Bitcoin; this will also take a while!"
+
+# CURRENT BITCOIN RELEASE:
+# Change as necessary
+export BITCOIN="bitcoin-core-0.20.1"
+
+export BITCOINPLAIN=`echo $BITCOIN | sed 's/bitcoin-core/bitcoin/'`
+
+sudo -u standup wget https://bitcoincore.org/bin/$BITCOIN/$BITCOINPLAIN-x86_64-linux-gnu.tar.gz -O ~standup/$BITCOINPLAIN-x86_64-linux-gnu.tar.gz
+sudo -u standup wget https://bitcoincore.org/bin/$BITCOIN/SHA256SUMS.asc -O ~standup/SHA256SUMS.asc
+sudo -u standup wget https://bitcoin.org/laanwj-releases.asc -O ~standup/laanwj-releases.asc
+
+# Verifying Bitcoin: Signature
+echo "$0 - Verifying Bitcoin."
+
+sudo -u standup /usr/bin/gpg --no-tty --import ~standup/laanwj-releases.asc
+export SHASIG=`sudo -u standup /usr/bin/gpg --no-tty --verify ~standup/SHA256SUMS.asc 2>&1 | grep "Good signature"`
+echo "SHASIG is $SHASIG"
+
+if [[ "$SHASIG" ]]
+then
+
+    echo "$0 - VERIFICATION SUCCESS / SIG: $SHASIG"
+
+else
+
+    (>&2 echo "$0 - VERIFICATION ERROR: Signature for Bitcoin did not verify!")
+
+fi
+
+# Verify Bitcoin: SHA
+export TARSHA256=`/usr/bin/sha256sum ~standup/$BITCOINPLAIN-x86_64-linux-gnu.tar.gz | awk '{print $1}'`
+export EXPECTEDSHA256=`cat ~standup/SHA256SUMS.asc | grep $BITCOINPLAIN-x86_64-linux-gnu.tar.gz | awk '{print $1}'`
+
+if [ "$TARSHA256" == "$EXPECTEDSHA256" ]
+then
+
+   echo "$0 - VERIFICATION SUCCESS / SHA: $TARSHA256"
+
+else
+
+    (>&2 echo "$0 - VERIFICATION ERROR: SHA for Bitcoin did not match!")
+
+fi
+
+# Install Bitcoin
+echo "$0 - Installinging Bitcoin."
+
+sudo -u standup /bin/tar xzf ~standup/$BITCOINPLAIN-x86_64-linux-gnu.tar.gz -C ~standup
+/usr/bin/install -m 0755 -o root -g root -t /usr/local/bin ~standup/$BITCOINPLAIN/bin/*
+/bin/rm -rf ~standup/$BITCOINPLAIN/
+
+echo "$0 - You can manually stop Bitcoin with: sudo systemctl stop bitcoind.service"
+echo "$0 - You can manually start Bitcoin with: sudo systemctl start bitcoind.service"
+
+# Finished, exit script
+exit 1
